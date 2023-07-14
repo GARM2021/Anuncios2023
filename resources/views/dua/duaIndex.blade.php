dc64f8b00ad4644114b2a78a8791cb74f04af1fa
GARM2021 <ivanhoe.real@gmail.com>
2023-07-14 16:39:53 -0600

    dua.index se fijan los h1

--- resources/views/dua/duaIndex.blade.php
+++ resources/views/dua/duaIndex.blade.php
@@ -1,60 +1,58 @@
- @extends('layouts.master')  {{-- //! Clase  30  --}} 
+ @extends('layouts.master') {{-- //! Clase  30  --}}
 
-@section('content')
-    
-
-    <h1> Lista de DUAS</h1>
-
-    @empty ($items)
-
-    <div class="alert alert-warning"> The list de Duas esta vacia</div>
+ @section('content')
 
 
-    @else
-      <h1>
-        
-        <a href="{{ route('duas.create')  }}" class="btn btn-success">Crea nuevo Dua</a>
-      </h1>
-        <div class="table-responsive">
-            <table class="table-stripped">
-                <thead class="thead-light">
-                    <tr>
-                        <th>DUA</th>
-                        <th>nomdua</th>
-                        <th>domdua</th>
-                        <th>colonia</th>
-                        <th>Nombre Colonia</th>
-                        <th>fechaini</th>
-                        <th>fechafin</th>
-                        <th>fbajax</th>
-                        <th>A C T I O N</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($items as $item ) 
-                        
-                  
-                    <tr class="highlight-on-hover">
-                        
-                        <td>{{ $item->dua}}</td>
-                        <td>{{ $item->nomdua}}</td>
-                        <td>{{ $item->domdua}}</td>
-                        <td>{{ $item->colonia}}</td>
-                        <td>{{ $item->nomcol}}</td> 
-                        <td>{{ $item->fechaini}}</td>
-                        <td>{{ $item->fechafin}}</td>
-                        <td>{{ $item->fbajax}}</td>
-                        <td> <a href="{{ route('duas.show', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}" class="btn btn-link"> Muestra </td>
-                        <td> <a href="{{ route('duas.edit', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}" class="btn btn-link"> Actualizaxx </td>
-                        <td> <a href="{{ route('subduas.lsubduas', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}" class="btn btn-link"> Lista SubDuas</td>  
-                      
+     <h1> Lista de DUAS</h1>
 
-                    </tr>
-                    
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    @endempty
-    @endsection
+     @empty($items)
+         <div class="alert alert-warning"> The list de Duas esta vacia</div>
+     @else
+         <h1>
 
+             <a href="{{ route('duas.create') }}" class="btn btn-success">Crea nuevo Dua</a>
+         </h1>
+         <div class="table-container table-responsive"> 
+
+             <table class="table-striped table-container">
+                
+                 <thead class="table-header-container">
+                     <tr>
+                         <th>DUA</th>
+                         <th>nomdua</th>
+                         <th>domdua</th>
+                         <th>colonia</th>
+                         <th>Nombre Colonia</th>
+                         <th>fechaini</th>
+                         <th>fechafin</th>
+                         <th>fbajax</th>
+                         <th>A C T I O N</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     @foreach ($items as $item)
+                         <tr class="highlight-on-hover table-row-container">
+
+                             <td>{{ $item->dua }}</td>
+                             <td>{{ $item->nomdua }}</td>
+                             <td>{{ $item->domdua }}</td>
+                             <td>{{ $item->colonia }}</td>
+                             <td>{{ $item->nomcol }}</td>
+                             <td>{{ $item->fechaini }}</td>
+                             <td>{{ $item->fechafin }}</td>
+                             <td>{{ $item->fbajax }}</td>
+                             <td> <a href="{{ route('duas.show', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}"
+                                     class="btn btn-link"> Muestra </td>
+                             <td> <a href="{{ route('duas.edit', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}"
+                                     class="btn btn-link"> Actualizaxx </td>
+                             <td> <a href="{{ route('subduas.lsubduas', ['dua' => str_pad($item->dua, 6, '0', STR_PAD_LEFT)]) }}"
+                                     class="btn btn-link"> Lista SubDuas</td>
+
+
+                         </tr>
+                     @endforeach
+                 </tbody>
+             </table>
+          </div> 
+     @endempty
+ @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,29 +8,84 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
+
+
+
+
+
         .highlight-on-hover:hover {
-            background-color: yellow;
+            background-color: yellow !important;
         }
+
+        .table-container {
+            max-height: 500px;
+            /* Ajusta la altura máxima según tus necesidades */
+            overflow-y: auto;
+        }
+
+        .table-header-container {
+            max-height: 50px;
+            /* Ajusta la altura máxima según tus necesidades */
+            overflow-y: auto;
+        }
+
+        .table-row-container {
+            max-height: 400px;
+            /* Ajusta la altura máxima según tus necesidades */
+            overflow-y: auto;
+        }
+
+        /* .header_fijo {
+  width: 1000px;
+  table-layout: sticky !important;
+  border-collapse: collapse; 
+}
+.header_fijo thead {
+  background-color: #333;
+  color: #FDFDFD;
+}
+
+.header_fijo thead tr {
+  display: block;
+  position: relative;
+}
+.header_fijo tbody {
+  display: block;
+  overflow: auto;
+  width: 100%;
+  height: 1000px;
+} */
+
+
+
+         .thead {
+            position: sticky !important;
+            top: 100;
+            background-color: #0b6fe2;
+            /* Ajusta el color de fondo según tu diseño */
+        }
+         
     </style>
-    
+
     <title>Anuncios2023_V1</title>
 </head>
 
 <body>
-  @if (isset($errors) && $errors->any())
-<div class="alert alert-danger">
-             
-        <ul>
-            @foreach ($errors->all() as $error)   {{-- //! Clase 39 --}}
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    @if (isset($errors) && $errors->any())
+        <div class="alert alert-danger">
 
-</div>
-      
-  @endif
+            <ul>
+                @foreach ($errors->all() as $error)
+                    {{-- //! Clase 39 --}}
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+
+        </div>
+
+    @endif
     @yield('content') {{-- //! Clase 30 --}}
 
 </body>
-            
+
 </html>
