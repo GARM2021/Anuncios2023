35932a2b6bc2ae0c7885104f8ed53a30db4874a9
GARM2021 <ivanhoe.real@gmail.com>
2023-07-31 17:06:52 -0600

    subduaCrea.blade header fijos

--- resources/views/subdua/subduaCrea.blade.php
+++ resources/views/subdua/subduaCrea.blade.php
@@ -1,134 +1,159 @@
 @extends('layouts.master')
 @section('content')
     {{-- //! Clase 31 --}}
-    <h1>Crea SUBDUA</h1>
+    <h4>Crea SUBDUA</h4>
 
     {{-- @dump($errors)
     @dump($dua) --}}
-    <h1>Dua {{ $dua }}</h1>
-    <h1>Dom. Dua {{ $nomdua }}</h1>
+    <div class="head_uno">
+        <h2>Dua {{ $dua }}</h2>
+        <h2>Dom. Dua {{ $nomdua }}</h2>
+    </div> <br>
 
 
 
 
 
 
+    {{-- <form method="POST" action="{{ route('subduas.store', ['dua' => $dua]) }}">  //! Clase  31 tenia action en lugar de method --}}
+    <form method="POST" action="{{ route('subduas.store') }}"> {{--  //! Clase  31 tenia action en lugar de method --}}
+        @csrf
+        @method('PUT') {{-- //! Clase  33 --}}
 
-    // <form method="POST" action="{{ route('subduas.store', ['dua' => $dua]) }}"> {{--  //! Clase  31 tenia action en lugar de method --}}
-        <form method="POST" action="{{ route('subduas.store') }}"> {{--  //! Clase  31 tenia action en lugar de method --}}
-            @csrf
-            @method('PUT') {{-- //! Clase  33 --}}
+        <input class="form-control highlight-on-hover_t" maxlength="60" type="hidden" readonly name="dua"
+            value={{ $dua }}>
 
-            <div class="form_row">
-
-                <input class="form-control" maxlength="60" type="hidden" readonly name="dua" value={{ $dua }}>
-            </div> <br>
-            <div class="form_row">
-
-                <div class="form_row">
-                    <label>Nom SubDua</label>
-                    <input class="form-control" maxlength="60" type="text" name="nomsubdua"
-                        value="{{ old('nomsubdua') }}" requiered>
-                </div> <br>
-                <div class="form_row">
-                    <label>Dom SubDua</label>
-                    <input class="form-control" maxlength="40" type="text" name="sububicaion"
-                        value="{{ old('sububicaion') }}" requiered>
-                    <br>
-                </div> <br>
-
-
-
-                {{-- <select id="ddlCol" class="ddl" onchange="this.form.submit()"> --}}
-                <select id="ddlCol" onchange="updateCvecol(this.value)">
-
-                    <option value="" selected>Selecciona Colonia</option>
-                    @foreach ($icolonias as $colonia)
-                        <option value="{{ $colonia->colonia }}">{{ $colonia->nomcol }}</option>
-                    @endforeach
-                    Colonia
-                </select>
-                <div class="form_row">
-                    <input class="form-control" type="hidden" name="colonia" value="{{ old('colonia') }}" requiered>
-                    <br>
-                </div> <br>
-
-                <div class="form_row">
-                    <label>Zona</label>
-                    <input class="form-control" maxlength="04" type="text" name="zona" value="{{ old('zona') }}"
-                        requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>subeexp</label>
-                    <input class="form-control" maxlength="08" type="text" name="subeexp" value="{{ old('subeexp') }}"
-                        requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>Tel. Subdua</label>
-                    <input class="form-control" maxlength="20" type="text" name="subtelefono"
-                        value="{{ old('subtelefono') }}" requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>Giro</label>
-                    <input class="form-control" maxlength="40" type="text" name="subdesgiro"
-                        value="{{ old('subdesgiro') }}" requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>Uso de Suelo</label>
-                    <input class="form-control" maxlength="40" type="text" name=" subusossuelo"
-                        value="{{ old('subusosuelo') }}" requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>RFC SubDua</label>
-                    <input class="form-control" maxlength="20" type="text" name="subrfc" value="{{ old('subrfc') }}"
-                        requiered>
-                    <br>
-                </div> <br>
+        <div class="head_uno">
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Nom SubDua</label>
+                <input class="form-control highlight-on-hover_t" maxlength="60" type="text" name="nomsubdua"
+                    value="{{ old('nomsubdua') }}" requiered>
                 <br>
-                <div class="form_row">
-                    <label>Nombre Propietario</label>
-                    <input class="form-control" maxlength="40" type="text" name="propnom" value="{{ old('propnom') }}"
-                        requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>Dom. Propietario</label>
-                    <input class="form-control" maxlength="40" type="text" name="propdir" value="{{ old('propdir') }}"
-                        requiered>
-                    <br>
-                </div> <br>
-                <div class="form_row">
-                    <label>Tel. Propietario</label>
-                    <input class="form-control" maxlength="20" type="text" name="proptel" value="{{ old('proptel') }}"
-                        requiered>
-                    <br>
-                </div> <br>
+            </div> <br>
 
-                <form action="/action_page.php">
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Dom SubDua</label>
+                <input class="form-control highlight-on-hover_t" maxlength="40" type="text" name="sububicaion"
+                    value="{{ old('sububicaion') }}" requiered>
+                <br>
+            </div> <br>
+        </div> <br>
+
+        <div class="div-container">
+
+            {{-- <select id="ddlCol" class="ddl" onchange="this.form.submit()"> --}}
+            <select id="ddlCol" onchange="updateCvecol(this.value)">
+
+                <option value="" selected>Selecciona Colonia</option>
+                @foreach ($icolonias as $colonia)
+                    <option value="{{ $colonia->colonia }}">{{ $colonia->nomcol }}</option>
+                @endforeach
+                Colonia
+            </select>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <input class="form-control highlight-on-hover_t" type="hidden" name="colonia" value="{{ old('colonia') }}"
+                    requiered>
+                <br>
+            </div> <br>
+
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Zona</label>
+                <input class="form-control highlight-on-hover_t" maxlength="04" type="text" name="zona"
+                    value="{{ old('zona') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>subeexp</label>
+                <input class="form-control highlight-on-hover_t" maxlength="08" type="text" name="subeexp"
+                    value="{{ old('subeexp') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Tel. Subdua</label>
+                <input class="form-control highlight-on-hover_t" maxlength="20" type="text" name="subtelefono"
+                    value="{{ old('subtelefono') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Giro</label>
+                <input class="form-control highlight-on-hover_t" maxlength="40" type="text" name="subdesgiro"
+                    value="{{ old('subdesgiro') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Uso de Suelo</label>
+                <input class="form-control highlight-on-hover_t" maxlength="40" type="text" name=" subusossuelo"
+                    value="{{ old('subusosuelo') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>RFC SubDua</label>
+                <input class="form-control highlight-on-hover_t" maxlength="20" type="text" name="subrfc"
+                    value="{{ old('subrfc') }}" requiered>
+                <br>
+            </div> <br>
+            <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Nombre Propietario</label>
+                <input class="form-control highlight-on-hover_t" maxlength="40" type="text" name="propnom"
+                    value="{{ old('propnom') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Dom. Propietario</label>
+                <input class="form-control highlight-on-hover_t" maxlength="40" type="text" name="propdir"
+                    value="{{ old('propdir') }}" requiered>
+                <br>
+            </div> <br>
+            <div tabindex="0" class="form_row highlight-on-hover_g">
+                <label>Tel. Propietario</label>
+                <input class="form-control highlight-on-hover_t" maxlength="20" type="text" name="proptel"
+                    value="{{ old('proptel') }}" requiered>
+                <br>
+            </div> <br>
+
+            {{-- <form action="/action_page.php" class="form_row highlight-on-hover_g">
+                <div tabindex="0" class="form_row highlight-on-hover_g">
                     <label for="fechabaja">Fecha Baja:</label><br><br>
-                    <input type="text" maxlength="08"id="fbajax" name="fbajax" placeholder="AAAAMMDD"
+                    <input class="form-control highlight-on-hover_t" maxlength="08"id="fbajax" name="fbajax" placeholder="AAAAMMDD"
                         pattern="[0-9]{8}" value="{{ old('fbajax') }}"><br><br>
                     <small>Format Ejemplo: 20230512</small><br><br>
-
-                </form>
-
-
-                <div class="form_row">
-                    <button type="submit" class="btn btn-primary btn-lg">Actualiza SUBDUA</button>
-
-                </div> <br>
+                    </div> <br>
+                </form> --}}
 
 
+            <div tabindex="0">
+                <button type="submit" class="btn btn-primary btn-lg">Crea SUBDUA</button>
 
-        </form>
-        <script>
-            function updateCvecol(value) {
-                document.getElementsByName("colonia")[0].value = value;
+            </div> <br>
+
+        </div> <br> 
+
+    </form>
+    <script>
+        window.onload = function() {
+
+            let cuenta = document.getElementById("idua").value;
+            cuenta = cuenta.padStart(6, '0');
+            document.getElementById("idua").value = cuenta;
+
+            const divs = document.querySelectorAll('div');
+            for (const div of divs) {
+                div.addEventListener('focus', function() {
+                    this.style.backgroundColor = 'rgb(123, 116, 133,1)';
+                    this.style.color = 'white !important';
+                });
+                div.addEventListener('blur', function() {
+                    this.style.backgroundColor = 'white';
+                    this.style.color = 'black !important';
+
+                });
             }
-        </script>
-    @endsection
+
+        }
+
+        function updateCvecol(value) {
+            document.getElementsByName("colonia")[0].value = value;
+        }
+    </script>
+@endsection
